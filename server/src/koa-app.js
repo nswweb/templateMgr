3e8457cd427e3bfff3f4619116d399b32a990d14
梁灿伦 <liangcl@nsw88.com>
2017-03-02 10:57:31 +0800

    添加async await

--- server/src/koa-app.js
+++ server/src/koa-app.js
@@ -1,11 +1,18 @@
+import "babel-polyfill";
 require('babel-core/register');
 const Koa = require('Koa');
 const app = new Koa();
+
+/*
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});*/
 
 app.use(async function (ctx, next) {
   const start = new Date();
   await next();
   const ms = new Date() - start;
+  ctx.body = 'cc' + 'Hello World';
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
